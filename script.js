2bee16fef262de7dca1c6d39695dbb3293cb4324
Cookiewookie87 <jernej.tratnik@gmail.com>
2018-07-10 15:32:07 +0200

    Dropdown

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const dropdownInput = document.querySelectorAll(".dropdown-input");
+const dropdownList = document.querySelectorAll(".dropdown-list");
 
 dropdownInput.forEach(input => input.addEventListener("click", openDropdown));
+dropdownInput.forEach(input => input.addEventListener("focusout", closeDropdown));
 
-
-function openDropdown() { 
-    console.log(this);
+function openDropdown() {
+    this.nextElementSibling.nextElementSibling.classList.toggle("active");
+}
+function closeDropdown() {
+    this.nextElementSibling.nextElementSibling.classList.remove("active");
 }
