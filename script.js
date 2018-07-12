a09286bd79a6bcb9e351483f57bf9a28b53ef88b
Cookiewookie87 <jernej.tratnik@gmail.com>
2018-07-12 15:16:27 +0200

    Dropdown fix
    Now semi working

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 const dropdownInput = document.querySelectorAll(".dropdown-input");
-const dropdownList = document.querySelectorAll(".dropdown-list");
+const dropdownInputArr = Array.from(document.querySelectorAll(".dropdown-input"));
+const dropdownList = document.querySelectorAll("ul");
+const dropdownListArr = Array.from(document.querySelectorAll("ul"));
+const currentListItems = document.querySelectorAll("li");
+let indexCurrentInput;
 
 dropdownInput.forEach(input => input.addEventListener("click", openDropdown));
+dropdownInput.forEach(input => input.addEventListener("focus", openDropdown));
 dropdownInput.forEach(input => input.addEventListener("focusout", closeDropdown));
+currentListItems.forEach(item => item.addEventListener("mousedown", listItemTextChange));
+currentListItems.forEach(item => item.addEventListener("mouseover", listItemHover));
+currentListItems.forEach(item => item.addEventListener("mouseout", listItemHoverOut));
 
 function openDropdown() {
-    this.nextElementSibling.nextElementSibling.classList.toggle("active");
+    indexCurrentInput = dropdownInputArr.indexOf(this);
+
+    dropdownList.item(indexCurrentInput).classList.add("activeList");
+
+    //console.log(dropdownInput.item(indexCurrentInput));
+    //console.log(dropdownList.item(indexCurrentInput));
+    //console.log(dropdownInput.item(indexCurrentInput).textContent);
+    //console.log(dropdownList.item(indexCurrentInput));
 }
-function closeDropdown() {
-    this.nextElementSibling.nextElementSibling.classList.remove("active");
+function closeDropdown(e) {
+    e.target.nextElementSibling.nextElementSibling.classList.remove("activeList");
+}
+function listItemHover() {
+    this.classList.add("hoverList");
+}
+function listItemHoverOut() {
+    this.classList.remove("hoverList");
+}
+function listItemTextChange() {
+    const currentInput = dropdownInput.item(indexCurrentInput);
+    if (this.textContent !== "Clear") { 
+        currentInput.textContent = this.textContent;
+        currentInput.classList.add("active-input");
+        this.classList.add("active-item");
+    } else {
+        currentInput.textContent = "Select item";
+        currentInput.classList.remove("active-input");
+        this.classList.remove("active-item");
+    }
 }
